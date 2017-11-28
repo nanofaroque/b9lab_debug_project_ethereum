6ae33670300d403b64a7d1096d0b301a2987bc53
Omar Faroque <faroque.ae@gmail.com>
2017-11-28 11:50:47 -0600

    updated piggy bank

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.4.5;
 
+
 contract PiggyBank {
     address owner;
+
     uint248 public balance;
+    /**
+     * tracker to detect the contract is active or not
+     */
+    bool public isActive;
 
     event LogFundsReceived(address sender, uint amount);
+
     event LogFundsSent(address receiver, uint amount);
+
     event LogKill(address killer);
 
     function piggyBank() public payable {
@@ -14,15 +22,18 @@ contract PiggyBank {
         LogFundsReceived(msg.sender, msg.value);
     }
 
-    function () payable public {
+    function() payable public {
+        require(isActive==true);
         require(msg.sender == owner);
         balance += uint248(msg.value);
+        isActive = true;
         LogFundsReceived(msg.sender, msg.value);
     }
 
     function kill(address killer) public {
         require(killer != owner);
         selfdestruct(owner);
+        isActive = false;
         LogKill(killer);
     }
 }
